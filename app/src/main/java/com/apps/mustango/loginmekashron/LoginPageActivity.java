a22ebbfd43de694d7211e6b5b37af895c9cc148a
Volodymyr Korzhovskyy <it.mustango@gmail.com>
2016-09-23 01:49:54 +0300

    worked EditText

--- app/src/main/java/com/apps/mustango/loginmekashron/LoginPageActivity.java
+++ app/src/main/java/com/apps/mustango/loginmekashron/LoginPageActivity.java
@@ -16,23 +16,17 @@ import org.ksoap2.serialization.SoapPrimitive;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
 
+import java.io.IOException;
+
 
 public class LoginPageActivity extends AppCompatActivity {
 
-
-    private static String SOAP_ACTION = "http://isapi.mekashron.com/StartAJob/General.dll/Login";
-    //private static String SOAP_ACTION = "http://isapi.mekashron.com/soapclient/soapclient.php";
-   // private static String SOAP_ACTION = "http://isapi.mekashron.com/soapclient/soapclient.php?URL=http://isapi.mekashron.com/StartAJob/General.dll%2Fwsdl%2FIGeneral";
-    private static String NAMESPACE = "http://isapi.mekashron.com/StartAJob/General.dll";
-  // private static String NAMESPACE ="http://www.w3.org/2003/05/soap-encoding";
+    private static String SOAP_ACTION = "http://isapi.mekashron.com/soapclient/soapclient.php?URL=http://isapi.mekashron.com/StartAJob/General.dll%2Fwsdl%2FIGeneral";
+    private static String NAMESPACE = "http://isapi.mekashron.com/StartAJob/General.dll/";
     private static String METHOD_NAME = "Login";
+    private static String URL = "http://isapi.mekashron.com/StartAJob/General.dll/wsdl/IGeneral";
 
- private static String URL = "http://isapi.mekashron.com/StartAJob/General.dll/wsdl/IGeneral";
-
-    EditText loginField;
-    EditText passwordField;
-  //  Post postRequest=new Post();
- //   RequestItemTask requestItemTask=null;
+    RequestItemTask requestItemTask=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,101 +37,75 @@ public class LoginPageActivity extends AppCompatActivity {
         final EditText passwordField=(EditText) findViewById(R.id.editText2);
         Button loginButton=(Button) findViewById(R.id.button);
 
-
-
         loginButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v){
-/*
-                String json = postRequest.loginJson(loginField.getText().toString(), passwordField.getText().toString());
-                Log.i("json string",json);
-                requestItemTask = (RequestItemTask) new RequestItemTask()
-                        .execute(json);
-*/
+            public void onClick(View v) {
 
-                new AsyncTask<Void, Void, Void>() {
-                    @Override
-                    protected Void doInBackground(Void... params) {
-                        try{
-
-                         /*  LoginService loginService =
-                                    ServiceGenerator.createService(LoginService.class, "Volodymyr", "qwerty123");
-                            Call<User> call = loginService.basicLogin();
-                            User user = call.execute().body();
-                            Log.i("request1",user.mName);
-                            Log.i("request2",user.mPassword);*/
-//Initialize soap request + add parameters
-
-                            SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
-                            request.addProperty("UserName","John")
-                                    .addProperty("Password","Candy")
-                                    .addProperty("IP","192.168.1.1");
-
-                            CustomSoapSerializationEnvelope envelope = new  CustomSoapSerializationEnvelope(CustomSoapEnvelop.VER12);
-                            envelope.setOutputSoapObject(request);
-                            //Needed to make the internet call
-                            HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
-                            try {
-                                //this is the actual part that will call the webservice
-                                androidHttpTransport.call(SOAP_ACTION, envelope);
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                            // Get the SoapResult from the envelope body.
-                            SoapObject result = (SoapObject)envelope.bodyIn;
-
-                            if(result != null){
-                                Log.i("response",result.getProperty(0).toString());
-                                //Get the first property and change the label text
-                            //    t.setText("SOAP response:\n\n" + result.getProperty(0).toString());
-                            }
-                        }catch(Exception e){
-                            //do something
-                        }
-                        return null;
-                    }
-                }.execute();
-
+                String[] par={loginField.getText().toString(),passwordField.getText().toString()};
+                requestItemTask = (RequestItemTask) new RequestItemTask().execute(par);
 
             }
         });
     }
+        private class RequestItemTask extends AsyncTask<String,Void, String> {
+
+
+            protected String doInBackground(String... string) {
+                String response = "";
+                String username=string[0];
+                String password=string[1];
+                //  postRequest.PostRequest("http://isapi.mekashron.com/StartAJob/General.dll",json);
+                try {
+                    SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
+                    request.addProperty("UserName",username)
+                            .addProperty("Password",password)
+                            .addProperty("IP","192.168.1.1");
+
+                    CustomSoapSerializationEnvelope envelope = new  CustomSoapSerializationEnvelope(CustomSoapEnvelop.VER12);
+                    envelope.setOutputSoapObject(request);
+                    //Needed to make the internet call
+                    HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
+                    try {
+                        //this is the actual part that will call the webservice
+                          androidHttpTransport.call(SOAP_ACTION, envelope);
+                          SoapObject result = (SoapObject)envelope.getResponse();
+                          if(result != null){
+                            Log.i("response",result.getProperty(0).toString());
+                            response=result.toString();
+                        }
+                        }catch (Exception e) {
+                                 e.printStackTrace();
+                    }
+                    // Get the SoapResult from the envelope body.
+                  //  SoapObject result = (SoapObject)envelope.bodyIn;
 
 
 
-
-
-
-
-    /*
-    private class RequestItemTask extends AsyncTask<String,Void, String> {
-
-
-        protected String doInBackground(String... string) {
-            String response = "";
-            String json=string[0];
-            //  postRequest.PostRequest("http://isapi.mekashron.com/StartAJob/General.dll",json);
-         /*   try {
-                response = postRequest.PostR("http://isapi.mekashron.com/StartAJob/General.dll/wsdl/IGeneral", json);
-                Log.i("request",response);
-                //  System.out.print(response);
-            } catch (IOException e) {
-                //обработка ошибки
+                }catch(Exception e){
+                    //do something
+                }
+                return response;
             }
-            return response;
-        }
 
-        @Override
-        protected void onPostExecute(String request) {
-            final TextView resultMessage = (TextView) findViewById(R.id.textView);
-            resultMessage.setText(request);
-            Log.i("request",request.toString());
-        }
+            @Override
+            protected void onPostExecute(String response) {
+                final TextView resultMessage = (TextView) findViewById(R.id.textView);
+                resultMessage.setText(response);
+                Log.i("response",response.toString());
+            }
 
+        }
     }
-*/
 
-}
+
+
+
+
+
+
+
+
+
 
 
 
